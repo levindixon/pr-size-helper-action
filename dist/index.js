83f5f7ddd3c9144746c31ba41e30c346cecbf4aa
Levin Dixon <levindixon@gmail.com>
2021-03-08 23:05:17 -0800

    update release

--- dist/index.js
+++ dist/index.js
@@ -7899,11 +7899,11 @@ const handleReasonComment = async (
   reasonCommentBody
 ) => {
   const existingIssues = await octokit.search.issuesAndPullRequests({
-    q: `is:open is:issue repo:${owner}/${repo} in:title [PR Size Helper]: Digest`,
+    q: `is:open is:issue repo:${owner}/${repo} in:title [ PR Size Helper ]: Digest`,
   });
 
   const existingIssue = existingIssues.data.items.find(
-    (issue) => issue.title === "[PR Size Helper]: Digest"
+    (issue) => issue.title === "[ PR Size Helper ]: Digest"
   );
 
   let newIssue;
@@ -7912,7 +7912,17 @@ const handleReasonComment = async (
     newIssue = await octokit.issues.create({
       owner,
       repo,
-      title: "[PR Size Helper]: Digest",
+      title: "[ PR Size Helper ]: Digest",
+      body: `Welcome to your PR Size Helper Digest!
+
+This issue collects and indexes all of the \`!reason\` comments captured in pull requests labeled as \`size/XL\` and \`size/XXL\`
+
+Here are some helpful links:
+
+All **open** PRs with a [\`size/XXL\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aopen+label%3Asize%2FXXL) [\`size/XL\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aopen+label%3Asize%2FXL) [\`size/L\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aopen+label%3Asize%2FL) [\`size/M\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aopen+label%3Asize%2FM) [\`size/S\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aopen+label%3Asize%2FS) [\`size/XS\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aopen+label%3Asize%2FXS)
+All **closed** PRs with a [\`size/XXL\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aclosed+label%3Asize%2FXXL) [\`size/XL\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aclosed+label%3Asize%2FXL) [\`size/L\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aclosed+label%3Asize%2FL) [\`size/M\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aclosed+label%3Asize%2FM) [\`size/S\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aclosed+label%3Asize%2FS) [\`size/XS\`](https://github.com/${owner}/${repo}/pulls?q=is%3Apr+is%3Aclosed+label%3Asize%2FXS)
+
+_Note: The title of this issue is important. If you decide to change it, the PR Size Helper action will create a new "[ PR Size Helper ]: Digest" issue the next time someone on your team creates a \`!reason\` comment._`,
     });
   }
 
